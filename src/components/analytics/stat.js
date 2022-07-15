c7a23b0943a75d29395bd1ecd60e10df4372bab3
NewUser <newuser@Mini.local>
2022-07-14 20:06:38 -0400

    /analytics tweaks

--- src/components/analytics/stat.js
+++ src/components/analytics/stat.js
@@ -18,29 +18,24 @@ return (
         flexDirection="column"
         w="100%"
         flex="1 0 30%"
-        px={{base: '4', md: '6'}}
-        py={{base: '5', md: '6'}}
+        px={['4']}
+        py={['5']}
         bg="gray.700"
         borderRadius="lg"
         boxShadow={useColorModeValue('sm', 'sm-dark')}
         m="2"
     >
-        <Flex minH="3rem" justifyContent="space-between" alignItems="center">
+        <Flex justifyContent="space-between" alignItems="center">
             <Text fontSize="sm" color="muted">
                 {label}
             </Text>
             <Icon onClick={() => console.log('clicked')} _hover={{cursor: 'pointer'}} as={FiMoreVertical} boxSize="5" color="muted" />
         </Flex>
-        <Flex justify="space-between">
-            <Heading
-                size={useBreakpointValue({
-                base: 'sm',
-                md: 'md',
-                })}
-            >
+        <Flex my="1" justify="space-between">
+            <Heading size={useBreakpointValue({base: 'sm', md: 'md'})}>
                 {value}
             </Heading>
-            <Badge variant="subtle" colorScheme={delta.isUpwardsTrend ? 'green' : 'red'}>
+            <Badge alignSelf="center" borderRadius="5px" variant="subtle" colorScheme={delta.isUpwardsTrend ? 'green' : 'red'}>
                 <HStack spacing="1">
                 <Icon as={delta.isUpwardsTrend ? FiArrowUpRight : FiArrowDownRight} />
                 <Text>{delta.value}</Text>
