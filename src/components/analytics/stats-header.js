869499f7f1de3c109600a591ad88432b896ac105
NewUser <newuser@Mini.local>
2022-07-16 07:41:00 -0400

    syncing branches

---
+++ src/components/analytics/stats-header.js
@@ -0,0 +1,39 @@
+import { Box, Flex, Container, SimpleGrid } from '@chakra-ui/react'
+import * as React from 'react'
+import { Stat } from './stat'
+const stats = [
+  {
+    label: 'Total Scorecards',
+    value: '143,887',
+    delta: {
+      value: '4.3%',
+      isUpwardsTrend: true,
+    },
+  },
+  {
+    label: 'Winner Avg. Rounds Won',
+    value: '74.87%',
+    delta: {
+      value: '2.3%',
+      isUpwardsTrend: true,
+    },
+  },
+  {
+    label: 'Correct Winner Predictions',
+    value: '63.87%',
+    delta: {
+      value: '0.1%',
+      isUpwardsTrend: false,
+    },
+  },
+]
+
+export const StatsHeader = () => (
+  <Box as="section" py={{base: '4'}}>
+    <Container maxWidth="100%">
+      <SimpleGrid columns={{base: 1, md: 3}} gap={{base: '5', md: '6'}}>
+        { stats.map( (stat, id) => <Stat key={id} {...stat} /> )}
+      </SimpleGrid>
+    </Container>
+  </Box>
+)
