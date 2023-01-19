1fa70309ae20361e4ffa559cafbbfedf26014fc6
NewUser <newuser@andrew.local>
2023-01-19 12:00:26 -0500

    pushing to new subdomained envs

--- deploy-ui.sh
+++ deploy-ui.sh
@@ -4,8 +4,8 @@ set -ueo pipefail
 ENV=${1}
 BUCKET="admin.fightsync.live-v3"
 
-if [ "$ENV" = "prod" ]; then
-    BUCKET="admin.fightsync.live-v3"
-fi
+# if [ "$ENV" = "prod" ]; then
+#     BUCKET="admin.fightsync.live-v3"
+# fi
 
 aws s3 sync ./build s3://${BUCKET} --delete
